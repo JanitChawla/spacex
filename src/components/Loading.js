e248e159a34f047610dd9bd5579ae20276eeafb9
JanitChawla <janitchawla31@gmail.com>
2023-01-13 17:08:25 +0530

    homepage added

---
+++ src/components/Loading.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <>
+      <div className="spinner">
+        <article></article>
+        <p className="text-white opacity-75 mt-5">
+          The spinny thing means it's loading
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Loading;
